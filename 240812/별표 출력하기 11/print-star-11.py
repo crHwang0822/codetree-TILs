fdb284150eb1afa8da133932b235dace36c3dcf4
Chaerin Hwang <87927105+crHwang0822@users.noreply.github.com>
2024-08-12 13:29:16 +0800

    Title: 별표 출력하기 11, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240812/별표 출력하기 11/print-star-11.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(2*n+1):
+    if i%2==0:
+        for _ in range(2*n+1):
+            print("*",end=' ')
+    else:
+        for _ in range(n+1):
+            print("*",end=' ')
+            print(" ",end=' ')
+    print()
